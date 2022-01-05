0e13f0e4140c1a56ae5f9370a62f83041e37b788
Raabia Ahmad <raabiaahmad.raabiasakeena@gmail.com>
2022-01-05 23:16:27 +0000

    Made a controller for entity class, Drink

--- Drinks/src/main/java/com/qa/springdrinks/service/ServiceMethods.java
+++ Drinks/src/main/java/com/qa/springdrinks/service/ServiceMethods.java
@@ -1,5 +1,21 @@
 package com.qa.springdrinks.service;
 
-public interface ServiceMethods {
+import java.util.List;
 
+public interface ServiceMethods<T>{
+
+//	Create
+	T create (T t);
+	
+//	Read by Id
+	T getById(long id);
+	
+//	Read All
+	List<T> getAll();
+	
+//	Update
+	T update(long id, T t);
+	
+//	Delete
+	boolean delete(long id);
 }
